860a52de03d8cef602b0226691a04bd6bfd00cc7
Victoria Demina <viktoriia.demina@gmail.com>
2023-05-01 22:29:25 +0100

    Add docs to Merchant.cs

--- src/PaymentGateway.Domain/Entities/Merchant.cs
+++ src/PaymentGateway.Domain/Entities/Merchant.cs
@@ -1,6 +1,13 @@
 namespace PaymentGateway.Domain.Entities;
 
+/// <summary>
+/// Represents a merchant that can receive payments.
+/// </summary>
 public class Merchant
 {
+    
+    /// <summary>
+    /// Unique identifier of the merchant.
+    /// </summary>
     public Guid Id { get; set; }
 }
