ffe8ab19ba34c2786b409f6000a4203f8c36993c
Victoria Demina <viktoriia.demina@gmail.com>
2023-05-01 22:36:07 +0100

    Add docs to enums

--- src/PaymentGateway.Domain/Entities/Transaction.cs
+++ src/PaymentGateway.Domain/Entities/Transaction.cs
@@ -2,11 +2,33 @@ using PaymentGateway.Domain.Enums;
 
 namespace PaymentGateway.Domain.Entities;
 
+/// <summary>
+/// Represents a payment transaction.
+/// </summary>
 public class Transaction
 {
+    /// <summary>
+    /// Unique identifier of the transaction.
+    /// </summary>
     public Guid Id { get; set; }
+    
+    /// <summary>
+    /// The merchant associated with the transaction.
+    /// </summary>
     public Merchant Merchant { get; set; }
+    
+    /// <summary>
+    /// Unique identifier of the merchant associated with the transaction.
+    /// </summary>
     public Guid MerchantId { get; set; }
+    
+    /// <summary>
+    /// Card number used for the transaction.
+    /// </summary>
     public string CardNumber { get; set; }
+    
+    /// <summary>
+    /// Status of the transaction.
+    /// </summary>
     public Status Status { get; set; }
 }
--- src/PaymentGateway.Domain/Enums/Status.cs
+++ src/PaymentGateway.Domain/Enums/Status.cs
@@ -1,5 +1,8 @@
 namespace PaymentGateway.Domain.Enums;
 
+/// <summary>
+/// Represents the status of a payment transaction: Pending, Success, or Failed
+/// </summary>
 public enum Status
 {
     Pending = 0,
