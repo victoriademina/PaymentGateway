f38c4ed5e62202c9baeaa85e08f95ce4f2c7337f
Victoria Demina <viktoriia.demina@gmail.com>
2023-05-01 22:32:35 +0100

    Add docs to PaymentAmount.cs

--- src/PaymentGateway.Domain/Entities/PaymentAmount.cs
+++ src/PaymentGateway.Domain/Entities/PaymentAmount.cs
@@ -1,7 +1,17 @@
 namespace PaymentGateway.Domain.Entities;
 
+/// <summary>
+/// Represents the amount and currency of a payment.
+/// </summary>
 public class PaymentAmount
 {
+    /// <summary>
+    /// The payment amount.
+    /// </summary>
     public decimal Amount { get; set; }
+    
+    /// <summary>
+    /// The payment currency.
+    /// </summary>
     public string Currency { get; set; }
 }
